fc4de8e2fe363e2b549fc7d6249426ee9ba925ed
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-10-04 10:52:36 -0300

    tela servicos

---
+++ src/telas/Servicos/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { FlatList, SafeAreaView, StatusBar, Text } from "react-native";
+
+const servicos = [
+  {
+    id: 1,
+    nome: "Banho",
+    preco: 79.9,
+    descricao: "NÃO DE BANHO NO SEU GATO!"
+  },
+  {
+    id: 2,
+    nome: "Vacina V4",
+    preco: 89.9,
+    descricao: "Uma dose da vacina V4. Seu gato precisa de duas. "
+  },
+  {
+    id: 3,
+    nome: "Vacina Antirrábica",
+    preco: 99.9,
+    descricao: "Uma dose da vacina antirrábica. Seu gato precisa de uma por ano."
+  },
+
+]
+
+export default function Servicos() {
+  return <SafeAreaView>
+    <StatusBar />
+    <Text>Servicos!</Text>
+    <FlatList
+      data={servicos}
+      renderItem={({ item: { nome } }) => <Text>{nome}</Text>}
+      keyExtractor={(id) => String(id)}
+    />
+  </SafeAreaView>
+}
